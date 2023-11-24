bbc189a50e86423ab0de3fac1b802d951d37456a
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-11-23 22:43:49 -0800

    Did task02 hp01

---
+++ hw01/mysecond-hw01.cc
@@ -0,0 +1,145 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/core-module.h"
+#include "ns3/csma-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/network-module.h"
+#include "ns3/point-to-point-module.h"
+
+// Default Network Topology
+//
+//       10.1.1.0
+// n0 -------------- n1   n2   n3   n4
+//    point-to-point  |    |    |    |
+//                    ================
+//                      LAN 10.1.2.0
+
+/*
+    Typical command to run the program,
+    see that the nCsma devices can be modified as shown in chp07
+
+    ./ns3 run "scratch/mysecond --nCsma=100"
+*/
+
+using namespace ns3;
+
+NS_LOG_COMPONENT_DEFINE("SecondScriptExample");
+
+int main(int argc, char *argv[])
+{
+    bool verbose = true;
+    uint32_t nCsma = 3;
+
+    CommandLine cmd(__FILE__);
+    cmd.AddValue("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+    cmd.AddValue("verbose", "Tell echo applications to log if true", verbose);
+
+    cmd.Parse(argc, argv);
+
+    if (verbose)
+    {
+        LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
+        LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
+
+    nCsma = nCsma == 0 ? 1 : nCsma;
+
+    NodeContainer p2pNodes;
+    p2pNodes.Create(2);
+
+    NodeContainer csmaNodes;
+    csmaNodes.Add(p2pNodes.Get(1));
+    csmaNodes.Create(nCsma);
+
+    PointToPointHelper pointToPoint;
+    pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
+    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+
+    NetDeviceContainer p2pDevices;
+    p2pDevices = pointToPoint.Install(p2pNodes);
+
+    CsmaHelper csma;
+    csma.SetChannelAttribute("DataRate", StringValue("100Mbps"));
+    csma.SetChannelAttribute("Delay", TimeValue(NanoSeconds(6560)));
+
+    NetDeviceContainer csmaDevices;
+    csmaDevices = csma.Install(csmaNodes);
+
+    InternetStackHelper stack;
+    stack.Install(p2pNodes.Get(0));
+    stack.Install(csmaNodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer p2pInterfaces;
+    p2pInterfaces = address.Assign(p2pDevices);
+
+    address.SetBase("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer csmaInterfaces;
+    csmaInterfaces = address.Assign(csmaDevices);
+
+    UdpEchoServerHelper echoServer(9);
+    UdpEchoServerHelper echoServer2(10); // Create Another echo server as per task02 hp01
+
+    ApplicationContainer serverApps = echoServer.Install(csmaNodes.Get(nCsma));
+    serverApps.Start(Seconds(1.0));
+    serverApps.Stop(Seconds(10.0));
+
+    // Create duplicate server app at rightmost node as per task02 hp01
+    // nCsma gets the total number of the csma nodes, which points at the rightmost node, refer to
+    // Doxygen
+    ApplicationContainer serverApps2 = echoServer2.Install(csmaNodes.Get(nCsma));
+    serverApps2.Start(Seconds(1.5));
+    serverApps2.Stop(Seconds(10.0));
+
+    UdpEchoClientHelper echoClient(csmaInterfaces.GetAddress(nCsma), 9);
+    echoClient.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient.SetAttribute("PacketSize", UintegerValue(1024));
+
+    // Make a duplicate echo client as per task02 hp01
+    // Note that 9 is the port number, not the number corresponding to the node, refer to Doxygen
+    UdpEchoClientHelper echoClient2(csmaInterfaces.GetAddress(nCsma), 9);
+    echoClient2.SetAttribute("MaxPackets", UintegerValue(1));
+    echoClient2.SetAttribute("Interval", TimeValue(Seconds(1.0)));
+    echoClient2.SetAttribute("PacketSize", UintegerValue(1024));
+
+    ApplicationContainer clientApps = echoClient.Install(p2pNodes.Get(0));
+    clientApps.Start(Seconds(2.0));
+    clientApps.Stop(Seconds(10.0));
+
+    // Make a duplicate app container for the second app to run
+    // 0 at the following line refers to the leftmost node p2p node, refer to Doxygen
+    ApplicationContainer clientApps2 = echoClient2.Install(p2pNodes.Get(0));
+    clientApps2.Start(Seconds(2.5)); // To ensure that it starts after the first app
+    clientApps2.Stop(Seconds(10.0));
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+
+    // Abridged during chp07 to leave the end result as shown below
+    // pointToPoint.EnablePcapAll("second");
+    // csma.EnablePcap("second", csmaDevices.Get(nCsma), true);
+    // csma.EnablePcap("second", csmaDevices.Get(nCsma - 1), false);
+    pointToPoint.EnablePcap("second", p2pNodes.Get(0)->GetId(), 0);
+    csma.EnablePcap("second", csmaNodes.Get(nCsma)->GetId(), 0, false);
+    csma.EnablePcap("second", csmaNodes.Get(nCsma - 1)->GetId(), 0, false);
+
+    Simulator::Run();
+    Simulator::Destroy();
+    return 0;
+}
