2d55f0499ce4e73a6e2e05db51acf883315a626c
Diego Cruz <61296683+diego-ruben-cruz@users.noreply.github.com>
2023-11-26 16:39:35 -0800

    Separated files into header and class

--- hw02/log-normal-shadowing-model.cc
+++ hw02/log-normal-shadowing-model.cc
@@ -3,37 +3,22 @@
  *
  */
 
-#include "ns3/applications-module.h"
+#include "log-normal-shadowing-model.h"
+
+#include "ns3/boolean.h"
 #include "ns3/core-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/mobility-module.h"
-#include "ns3/network-module.h"
-#include "ns3/point-to-point-module.h"
+#include "ns3/double.h"
+#include "ns3/log.h"
+#include "ns3/mobility-model.h"
+#include "ns3/pointer.h"
 #include "ns3/propagation-loss-model.h"
+#include "ns3/string.h"
+
+#include <cmath>
 
 using namespace ns3;
 
-// Borrowing some of the conventions of making a class from propagation-loss-model.cc
-class LogNormalShadowingModel : public PropagationLossModel
-{
-public:
-    LogNormalShadowingModel();
-    virtual ~LogNormalShadowingModel();
-
-    static TypeId GetTypeId();
-
-    // overriding default methods to add Gaussian Random Var to the mix
-    virtual double GetLoss(double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b);
-    virtual Ptr<PropagationLossModel> Copy() const;
-
-    void SetPathLossExponent(double exponent);
-    void SetNoiseVariance(double variance);
-
-private:
-    // internal-use vars for loss exponent and noise variance
-    double m_pathLossExponent;
-    double m_noiseVariance;
-};
+NS_LOG_COMPONENT_DEFINE("LogNormalShadowingModel");
 
 // Making implementation for the previously-created class, still using propagation-loss-model.cc
 NS_OBJECT_ENSURE_REGISTERED(LogNormalShadowingModel);
@@ -42,104 +27,87 @@ TypeId
 LogNormalShadowingModel::GetTypeId()
 {
     static TypeId tid =
-        TypeId("LogNormalShadowingModel")
+        TypeId("ns3::LogNormalShadowingModel")
             .SetParent<PropagationLossModel>()
             .SetGroupName("Propagation")
             .AddConstructor<LogNormalShadowingModel>()
-            .AddAttribute("Exponent",
-                          "The exponent of the Path loss propagation model",
-                          DoubleValue(2.8),
+            .AddAttribute("pathLossExponent",
+                          "The exponent of the Path Loss propagation model",
+                          DoubleValue(2.5),
                           MakeDoubleAccessor(&LogNormalShadowingModel::m_pathLossExponent),
                           MakeDoubleChecker<double>())
-            .AddAttribute("Noise Variance",
-                          "The random variable to represent noise",
-                          DoubleValue(4),
-                          MakeDoubleAccessor(&LogNormalShadowingModel::m_noiseVariance),
-                          MakeDoubleChecker<double>());
+            .AddAttribute("refDistance",
+                          "The distance at which the reference loss is calculated (m)",
+                          DoubleValue(1.0),
+                          MakeDoubleAccessor(&LogNormalShadowingModel::m_refDistance),
+                          MakeDoubleChecker<double>())
+            .AddAttribute("refLoss",
+                          "The reference loss at reference distance (dB). (Default is Friis at 1m "
+                          "with 5.15 GHz)",
+                          DoubleValue(46.6777),
+                          MakeDoubleAccessor(&LogNormalShadowingModel::m_refLoss),
+                          MakeDoubleChecker<double>())
+            .AddAttribute("gaussRandomVar",
+                          "The random Gaussian Variable",
+                          StringValue("ns3::NormalRandomVariable[Mean=0|Variance=0]"),
+                          MakePointerAccessor(&LogNormalShadowingModel::m_gaussRandomVariable),
+                          MakePointerChecker<RandomVariableStream>());
+
     return tid;
 }
 
+LogNormalShadowingModel::LogNormalShadowingModel()
+{
+}
+
+void LogNormalShadowingModel::SetPathLossExponent(double n)
+{
+    m_pathLossExponent = n;
+}
+
+void LogNormalShadowingModel::SetReference(double referenceDistance, double referenceLoss)
+{
+    m_refDistance = referenceDistance;
+    m_refLoss = referenceLoss;
+}
+
 double
-LogNormalShadowingModel::GetLoss(double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b)
+LogNormalShadowingModel::GetPathLossExponent() const
 {
-    // Create LogDistancePropagationLossModel internally, to later add the Gauss random var
-    Ptr<LogDistancePropagationLossModel> logDistLoss =
-        CreateObject<LogDistancePropagationLossModel>();
-    logDistLoss->SetPathLossExponent(m_pathLossExponent);
-
-    // Calculate path loss thru the conventional method
-    double pathLoss = logDistLoss->CalcRxPower(txPowerDbm, a, b);
-
-    // Generate a random variable with log-normal distribution
-    Ptr<NormalRandomVariable> normalRandVar = CreateObject<NormalRandomVariable>();
-    normalRandVar->SetAttribute("Mean", DoubleValue(0.0));
-    normalRandVar->SetAttribute("Variance", DoubleValue(m_noiseVariance));
-
-    // Add the log-normal shadowing effect
-    double shadowEffect = normalRandVar->GetValue();
-    double totalLoss = pathLoss + shadowEffect;
-
-    return totalLoss;
+    return m_pathLossExponent;
 }
 
-Ptr<PropagationLossModel>
-LogNormalShadowingModel::Copy() const
+double
+LogNormalShadowingModel::DoCalcRxPower(double txPowerDbm,
+                                       Ptr<MobilityModel> a,
+                                       Ptr<MobilityModel> b) const
 {
-    return CreateObject<LogNormalShadowingModel>(*this);
-}
+    double distance = a->GetDistanceFrom(b);
+    if (distance < -m_refDistance)
+    {
+        return txPowerDbm - m_refLoss;
+    }
 
-void LogNormalShadowingModel::SetPathLossExponent(double exponent)
-{
-    m_pathLossExponent = exponent;
-}
+    /**
+     * Copying log distance prop loss formula is:
+     * rx = 10 * log (Pr0(tx)) - n * 10 * log (d/d0)
+     *
+     * Pr0: rx power at reference distance d0 (W)
+     * d0: reference distance: 1.0 (m)
+     * d: distance (m)
+     * tx: tx power (dB)
+     * rx: dB
+     *
+     * Which becomes modified to:
+     *
+     * rx = rx0(tx) - 10 * n * log (d/d0)
+     */
 
-void LogNormalShadowingModel::SetNoiseVariance(double variance)
-{
-    m_noiseVariance = variance;
-}
+    double pathlossDbm = 10 * m_pathLossExponent * std::log10(distance / m_refDistance);
+    double receivedPower = -m_refLoss - pathlossDbm;
+    double gaussLoss = m_gaussRandomVariable->GetValue();
 
-int main(int argc, char *argv[])
-{
-    // borrowing some lines from mythird-hw01.cc
-    double lossExp = 2.8;
-    double noiseVar = 4;
-
-    CommandLine cmd(__FILE__);
-    cmd.AddValue("lossExp", "The loss exponent", lossExp);
-    cmd.AddValue("noiseVar", "The random variable for the noise", noiseVar);
-
-    cmd.Parse(argc, argv);
-
-    // Create nodes
-    NodeContainer nodes;
-    nodes.Create(2);
-
-    // Set fixed positions for the nodes
-    Ptr<MobilityModel> mobilityA = CreateObject<ConstantPositionMobilityModel>();
-    Ptr<MobilityModel> mobilityB = CreateObject<ConstantPositionMobilityModel>();
-
-    nodes.Get(0)->AggregateObject(mobilityA);
-    nodes.Get(1)->AggregateObject(mobilityB);
-
-    // Set positions
-    Vector posA(0, 0, 0);
-    Vector posB(100, 0, 0);
-
-    mobilityA->SetPosition(posA);
-    mobilityB->SetPosition(posB);
-
-    // Create point-to-point channel
-    PointToPointHelper pointToPoint;
-    pointToPoint.SetDeviceAttribute("DataRate", StringValue("100Mbps"));
-    pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
-
-    NetDeviceContainer devices = pointToPoint.Install(nodes);
-
-    // Need to set the model here then do the path loss computing
-
-    // Run the simulation
-    Simulator::Run();
-    Simulator::Destroy();
-
-    return 0;
+    NS_LOG_DEBUG("distance=" << distance << "m, reference-attenuation=" << -m_refLoss << "dB, "
+                             << "attenuation coefficient=" << receivedPower << "db");
+    return txPowerDbm + receivedPower + gaussLoss;
 }
---
+++ hw02/log-normal-shadowing-model.h
@@ -0,0 +1,45 @@
+#ifndef LOG_NORMAL_SHADOWING_MODEL_H
+#define LOG_NORMAL_SHADOWING_MODEL_H
+
+#include "ns3/object.h"
+#include "ns3/random-variable-stream.h"
+#include "ns3/propagation-loss-model.h"
+#include <map>
+
+namespace ns3
+{
+
+    // Borrowing some of the conventions of making a class from propagation-loss-model.cc
+    class LogNormalShadowingModel : public PropagationLossModel
+    {
+    public:
+        static TypeId GetTypeId(); // Returns object TypeId
+        LogNormalShadowingModel();
+
+        // Removes the copy() bit and assignment op to avoid wronguse
+        LogNormalShadowingModel(const LogNormalShadowingModel &) = delete;
+        LogNormalShadowingModel &operator=(const LogNormalShadowingModel &) = delete;
+
+        // SetGet combo for the path loss exponent
+        void SetPathLossExponent(double exponent);
+        double GetPathLossExponent() const;
+
+        // Sets the ref path loss at a given distance
+        void SetReference(double refDistance, double refLoss);
+
+    private:
+        // internal-use vars for loss exponent and noise variance
+        double m_pathLossExponent; // Exponent provided for the model
+        double m_refDistance;      // Initial distance that corresponds to Reference Path Loss
+        double m_refLoss;          // Path loss at reference distance
+        Ptr<RandomVariableStream> m_gaussRandomVariable;
+
+        double DoCalcRxPower(double txPowerDbm, Ptr<MobilityModel> a, Ptr<MobilityModel> b) const override;
+
+        int64_t DoAssignStreams(int64_t stream) override;
+
+        // Creates a default reference path loss/distance in case they are not provided
+        static Ptr<PropagationLossModel> CreateDefaultReference();
+    };
+}
+#endif
